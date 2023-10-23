e06f9346b872a4968ff57336af4b9ae9ecac9358
SLeBron <sophieclebron@gmail.com>
2023-10-23 10:40:29 -0600

    started index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+let gameMessage = confirm("Hi! Let's play rock, paper, scissors!");
 
-import confetti from 'canvas-confetti';
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+let game = function() {
+let userChoice = prompt("Please enter rock, paper, or scissors");
+console.log(userChoice);
+
+let choices = ["rock", "paper", "scissors"];
+let random = Math.floor(Math.random() * choices.length);
+console.log(random, choices[random]);
+let compChoice = alert("The computer chose " + choices[random]);
+
+if (userChoice === choices[random]) {
+    alert("It's a tie!");
+    ties++;
+} 
+else if (userChoice === "rock" && choices[random] === "paper") {
+    alert("You lost :(");
+    losses++;
+} 
+else if (userChoice === "paper" && choices[random] === "scissors") {
+    alert("You lost :(");
+    losses++;
+}
+else if (userChoice === "scissors" && choices[random] === "rock") {
+    alert("You lost :(");
+    losses++;
+}
+else {
+    alert("You won!!! :)");
+    wins++;
+}
+
+alert(
+    "Scoreboard" + "\n" + "Wins: " + wins + "\n" + "Losses: " + losses + "\n" + "Ties: " + ties);
+
+var playAgain = confirm("Do you want to play again?")
+
+if (playAgain) {
+    game();
+}
+}
+game ()
